c0758968e08a2438885260290857d35968c83bd0
SunJ <jsun@bouffalolab.com>
2024-06-11 18:39:00 +0800

    Migration to v4.0
    Signed-off-by: SunJ <jsun@bouffalolab.com>

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -13,11 +13,11 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      autopairs = true,                                 -- enable autopairs at start
+      cmp = true,                                       -- enable completion at start
+      diagnostics_mode = 3,                             -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true,                              -- highlight URLs at start
+      notifications = true,                             -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -26,14 +26,17 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        number = true,         -- sets vim.opt.number
+        spell = false,         -- sets vim.opt.spell
+        signcolumn = "auto",   -- sets vim.opt.signcolumn to yes
+        wrap = false,          -- sets vim.opt.wrap
+        tabstop = 2,           -- number of space in a tab
+        shiftwidth = 2,        -- number of space inserted for indentation
+        expandtab = true,      -- enable the use of space in tab
       },
-      g = { -- vim.g.<key>
+      g = {                    -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
@@ -47,8 +50,8 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["L"]          = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["H"]          = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
@@ -60,12 +63,75 @@ return {
           desc = "Close buffer from tabline",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- tables with the `name` key will be registered with which-key if it's installed
         -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<leader>b"]  = { name = "Buffers" },
+        ["<leader>g"]  = { name = "Git" },
 
+        -- Neotree
+        ["<leader>e"]  = false,
+        ["<F4>"]       = { "<cmd>Neotree toggle<CR>", desc = "Toggle Explorer" },
+        ["<leader>o"]  = { "<cmd>Neotree toggle<CR>", desc = "Toggle Explorer" },
+        ["<F8>"]       = {
+          function()
+            require("telescope.builtin").find_files { hidden = true, no_ignore = true }
+          end,
+          desc = 'Find all files',
+        },
+
+        -- tables with the `name` key will be registered with which-key if it's installed
+        -- quick save
+        ["<C-s>"]      = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+
+        --spectre
+        ["<leader>s"]  = { name = "Spectre" },
+        ["<leader>S"]  = {
+          ":lua require(\'spectre\').open()<CR>",
+          desc = "Open spectre search"
+        },
+        ["<leader>sw"] = {
+          ":lua require(\'spectre\').open_visual({select_word=true})<CR>",
+          desc = "Search current word"
+        },
+        ["<leader>sp"] = {
+          "viw:lua require(\'spectre\').open_file_search()<CR>",
+          desc = "Search current buffer"
+        },
+
+        -- close buffer
+        ["<leader>c"]  = false,
+        ["<leader>x"]  = {
+          function() require("astronvim.utils.buffer").close() end, desc = "Close buffer"
+        },
+        ["<leader>X"]  = {
+          function() require("astronvim.utils.buffer").close(0, true) end, desc = "Force close buffer"
+        },
+
+        --Lspsaga
+        ["<leader>lk"] = { "<cmd>Lspsaga hover_doc<CR>", desc = "Lspsaga Hover doc" },
+        ["<leader>lg"] = { "<cmd>Lspsaga goto_definition<CR>", desc = "Lspsaga Goto definition" },
+        ["<leader>lp"] = { "<cmd>Lspsaga peek_definition<CR>", desc = "Lspsaga Preview definition" },
+        ["<leader>lF"] = { "<cmd>Lspsaga finder<CR>", desc = "Lspsaga finder" },
+        ["<leader>lS"] = { "<cmd>Lspsaga outline<CR>", desc = "Lspsaga Symbols outline" },
+      },
+
+      i = {
+        ["<C-s>"] = { "<C-c>:w!<CR>", desc = "Save File while insert" }, -- change description but the same command
+      },
+
+      t = {
         -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<esc>"] = false,
+      },
+
+      v = {
+        ["<leader>s"] = {
+          ":lua require(\'spectre\').open_visual()<CR>",
+          desc = "Search current block"
+        },
+
+        -- clangformat
+        -- ["<leader>lm"] = { "<cmd>py3f /home/jokeo/tool/clang/tools/clang-format/clang-format.py<CR>", desc = "format cpp by clang format" },
       },
     },
   },
