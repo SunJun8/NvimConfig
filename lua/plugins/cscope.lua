c28cb3e57594ca3cd8bfbe190bb0d280ba982eb0
SunJ <jsun@bouffalolab.com>
2024-06-11 19:15:13 +0800

    fix conflicting keys
    Signed-off-by: SunJ <jsun@bouffalolab.com>

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -10,7 +10,7 @@ return {
     }
   },
   opts = {
-    prefix = "<leader>c",                   -- prefix to trigger maps
+    prefix = "<leader>x",                   -- prefix to trigger maps
     disable_maps = false,                   -- true disables my keymaps, only :Cscope will be loaded
     cscope = {
       db_file = "./cscope.out",             -- location of cscope db file
