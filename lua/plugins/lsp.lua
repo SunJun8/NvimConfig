a965d5c969e2e2053f20b67c25be1c612c5cd7c8
SunJ <jsun@bouffalolab.com>
2024-06-12 14:02:28 +0800

    set fzf lazy load

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,36 +1,34 @@
-  return {
-    {
-      "ojroques/nvim-lspfuzzy",
-      event = "User AstroFile",
-      dependencies = {
-        {
-          "junegunn/fzf",
-          event = "BufEnter",
-        },
-        {
-          "junegunn/fzf.vim",
-          event = "BufEnter",
-        }
+return {
+  {
+    "ojroques/nvim-lspfuzzy",
+    event = "User AstroFile",
+    dependencies = {
+      {
+        "junegunn/fzf",
       },
-    },
-    {
-      'nvimdev/lspsaga.nvim',
-      event = "LspAttach",
-      config = function()
-        require('lspsaga').setup({
-          definition = {
-            width = 0.6,
-            height = 0.6,
-          },
-          outline = {
-            win_width = 40,
-          },
-        })
-      end,
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-tree/nvim-web-devicons',
+      {
+        "junegunn/fzf.vim",
       }
+    },
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    event = "LspAttach",
+    config = function()
+      require('lspsaga').setup({
+        definition = {
+          width = 0.6,
+          height = 0.6,
+        },
+        outline = {
+          win_width = 40,
+        },
+      })
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
     }
-
   }
+
+}
