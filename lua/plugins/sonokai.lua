4143cc479850d0902265ec922c65444587bdc406
SunJ <jsun@bouffalolab.com>
2024-07-16 14:38:20 +0800

    fix colorscheme error
    Signed-off-by: SunJ <jsun@bouffalolab.com>

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -10,20 +10,32 @@ return {
 
   {
     "sainnhe/sonokai",
-    dependencies = {
-      "AstroNvim/astrocore",
-      opts = {
-        options = {
-          g = {
-            sonokai_style = 'espresso',
-            sonokai_enable_italic = false,
-            sonokai_disable_italic_comment = true,
-            sonokai_diagnostic_text_highlight = false,
-            sonokai_transparent_background = 2,
-            sonokai_dim_inactive_windows = 1,
-          }
-        }
-      },
-    },
+    lazy = false,
+    priority = 1000,
+    -- dependencies = {
+    --   "AstroNvim/astrocore",
+    --   opts = {
+    --     options = {
+    --       g = {
+    --         sonokai_style = 'espresso',
+    --         sonokai_enable_italic = false,
+    --         sonokai_disable_italic_comment = true,
+    --         sonokai_diagnostic_text_highlight = false,
+    --         sonokai_transparent_background = 2,
+    --         sonokai_dim_inactive_windows = 1,
+    --       }
+    --     }
+    --   },
+    -- },
+    config = function()
+      -- directly inside the plugin declaration.
+      -- vim.cmd.colorscheme('sonokai')
+      vim.g.sonokai_style = 'espresso'
+      vim.g.sonokai_enable_italic = false
+      vim.g.sonokai_disable_italic_comment = true
+      vim.g.sonokai_diagnostic_text_highlight = false
+      vim.g.sonokai_transparent_background = 2
+      vim.g.sonokai_dim_inactive_windows = 1
+    end
   },
 }
