6499536bcc2314827b50a13c8f5ea5445369eb1a
SunJ <jsun@bouffalolab.com>
2025-04-08 10:00:30 +0800

    fix astronvim 5
    Signed-off-by: SunJ <jsun@bouffalolab.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,19 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    dashboard = {
+  opts = function(_, opts)
+    local get_icon = require("astroui").get_icon
+    opts.dashboard = {
     preset = {
+        keys = {
+          { key = "n", action = "<Leader>n", icon = get_icon("FileNew", 0, true), desc = "New File  " },
+          { key = "f", action = "<Leader>fF", icon = get_icon("Search", 0, true), desc = "Find File  " },
+          { key = "r", action = "<Leader>fo", icon = get_icon("DefaultFile", 0, true), desc = "Recents  " },
+          { key = "w", action = "<Leader>fw", icon = get_icon("WordFile", 0, true), desc = "Find Word  " },
+          { key = "u", action = ":Lazy update", icon = get_icon("Refresh", 0, true), desc = "Update  " },
+          { key = "q", action = ":q", icon = get_icon("ArrowLeft", 0, true), desc = "Exit  " },
+          -- { key = "'", action = "<Leader>f'", icon = get_icon("Bookmarks", 0, true), desc = "Bookmarks  " },
+          -- { key = "s", action = "<Leader>Sl", icon = get_icon("Refresh", 0, true), desc = "Last Session  " },
+        },
       header = table.concat(
       	{
 				"██   ██ ███    ██  ██████  ██     ██ ██      ███████ ██████   ██████  ███████",
@@ -18,6 +29,6 @@ return {
 				"     ██   ██ ███████     ██   ██  ██████    ██    ██  ██████  ██   ████",
       	}, "\n"),
       },
-    },
-  },
+    }
+  end,
 }
